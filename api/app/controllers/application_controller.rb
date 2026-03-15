ff97bd300e3665c0bdbd384a724e85050a6e01f9
Michael Luna <michaelluna244@gmail.com>
2026-03-14 22:20:51 -0700

    remove jwt logic and replace it with sessions

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,29 +1,13 @@
 class ApplicationController < ActionController::Base
+  inertia_share current_user: -> { current_user&.slice(:id, :first_name, :last_name, :email, :profile_picture) }
+
   private
 
-  def authenticate_user!
-    token = extract_token_from_header
-    
-    unless token
-      return render json: { error: 'No token provided' }, status: :unauthorized
-    end
-    
-    begin
-      decoded = JsonWebToken.decode(token)
-      @current_user = User.find(decoded['user_id'])
-    rescue StandardError => e
-      render json: { error: e.message }, status: :unauthorized
-    end
-  end
-
   def current_user
-    @current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def extract_token_from_header
-    authorization_header = request.headers['Authorization']
-    return nil unless authorization_header
-    
-    authorization_header.split(' ').last
+  def require_login!
+    redirect_to root_path unless current_user
   end
 end

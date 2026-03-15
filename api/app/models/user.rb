f384510be38ed9aed0b5c0337bbd915b03f881a6
Michael Luna <michaelluna244@gmail.com>
2026-03-14 22:09:08 -0700

    Remove api v1 controllers and remove JWT functionality

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ApplicationRecord
   validates :strava_id, presence: true, uniqueness: true
-
-  def generate_jwt
-    payload = { user_id: id, exp: 24.hours.from_now.to_i }
-    JsonWebToken.encode(payload)
-  end
 end
--- api/app/serializers/user_serializer.rb
+++
@@ -1,5 +0,0 @@
-class UserSerializer
-  include JSONAPI::Serializer
-  
-  attributes :strava_id, :email, :first_name, :last_name, :profile_picture
-end

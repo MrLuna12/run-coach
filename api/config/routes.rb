8a601032c6ff0e8a5a020ceaca1ca04aeac671f7
Michael Luna <michaelluna244@gmail.com>
2026-03-14 22:20:02 -0700

    Refactor routes to simplify authentication endpoints and set new root path. Removed Inertia routes and updated health check endpoint.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,25 +1,12 @@
 Rails.application.routes.draw do
-
   # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
   constraints(host: "127.0.0.1") do
     get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
   end
-  root 'inertia_example#index'
-  get 'inertia-example', to: 'inertia_example#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  # 
-  namespace :api do
-    namespace :v1 do
-      post 'auth/strava', to: 'auth#strava_callback'
-      get 'auth/me', to: 'auth#me'
-      delete 'auth/logout', to: 'auth#destroy'
-    end
-  end
+  get  "auth/strava/callback", to: "auth#strava_callback"
+  get  "auth/logout",          to: "auth#logout"
+  root "auth#index"
 end
